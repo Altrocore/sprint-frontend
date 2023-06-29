cc8d37e7fc31e4798d0b8ca27eaf6bf1d87b8f05
Daniel <93489366+Altrocore@users.noreply.github.com>
2023-06-29 12:22:19 -0230

    add validations for login form

--- src/components/login-page/Login.js
+++ src/components/login-page/Login.js
@@ -4,17 +4,54 @@ import { useState } from 'react';
 import { useMyContext } from '../../Context'
 function Login({changeIsLogged}) {
 
+  const loginRegex = /^[a-zA-Z0-9_]{3,20}$/; 
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/; 
+
+  const [errors, setErrors] = useState({
+    login: '',
+    password: '',
+  });
+
+  const [isFormValid, setIsFormValid] = useState(false);
+
   const [form, setForm] = useState({
     login: '',
     password: '',
   });
   const { login, updateContext } = useMyContext();
   const onUpdateField = e => {
+    const {name, value} = e.target;
+    let error = '';
+
+    if (name === 'login') {
+      if(!loginRegex.test(value)) {
+        error = 'Login must contain from 3 to 20 characters: letters, numbers or underscores';
+      }
+    } else if (name === 'password') {
+      if (!passwordRegex.test(value)) {
+        error = 'Password must contain at least 8 characters, at least one letter and one number';
+      }
+    }
+
+    setErrors(prevErrors => ({
+      ...prevErrors,
+      [name]: error,
+    }));
+
     const nextFormState = {
       ...form,
-      [e.target.name]: e.target.value,
+      [name]: value,
     };
     setForm(nextFormState);
+
+    validateForm();
+  };
+
+  const validateForm = () => {
+    const { login, password } = form;
+    const isLoginValid = loginRegex.test(login);
+    const isPasswordValid = passwordRegex.test(password);
+    setIsFormValid(isLoginValid && isPasswordValid);
   };
 
   const saveData = () => {
@@ -24,7 +61,9 @@ function Login({changeIsLogged}) {
 
   const onSubmitForm = e => {
     e.preventDefault();
-    saveData();
+    if (isFormValid) {
+      saveData();
+    }
   };
 
   return (
@@ -34,7 +73,9 @@ function Login({changeIsLogged}) {
       </div>
       <form className='login-form' onSubmit={onSubmitForm}>
         <input onChange={onUpdateField} value={form.login} type='text' name='login' className='login-inp' placeholder='Login'/>
+        {errors.login && <span className='error-msg'>{errors.login}</span>}
         <input onChange={onUpdateField} value={form.password} type='password' name='password' className='login-inp' placeholder='Password'/>
+        {errors.password && <span className='error-msg'>{errors.password}</span>}
         <button type='submit' className='sign-btn'>Sign in</button>
       </form>
     </div>
